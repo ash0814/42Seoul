d75eb8ee3e78e49dba0320512bb6236bfe7bdbcd
안세현 <sehyeon0814@naver.com>
2021-05-20 16:43:18 +0900

    안한거 빼고 다함

--- srcs/camera.c
+++ srcs/camera.c
@@ -6,27 +6,39 @@
 /*   By: sehyan <sehyan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/17 01:54:02 by ash               #+#    #+#             */
-/*   Updated: 2021/05/18 14:40:48 by sehyan           ###   ########.fr       */
+/*   Updated: 2021/05/20 16:35:10 by sehyan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minirt.h"
 
-t_camera	*camera(t_canvas canvas, t_point orig, t_vec nor, double fov)
+t_camera	*camera(t_canvas canvas, t_point org, t_vec nor, double fov)
 {
 	t_camera	*cam;
+	t_vec		vup;
 
 	cam = (t_camera *)malloc(sizeof(t_camera));
 	cam->fov = fov;
-	cam->org = orig;
+	cam->org = org;
 	cam->viewport_w = 2 * tan(cam->fov * 3.14 / 360.0);
 	cam->viewport_h = cam->viewport_w / canvas.aspect_ratio;
 	cam->focal_len = 1.0;
-	cam->hrzt = vec(cam->viewport_w, 0, 0);
-	cam->vrtc = vec(0, cam->viewport_h, 0);
 	cam->normal = nor;
-	cam->left_bottom = v_sub(v_sub(v_sub(cam->org, v_div(cam->hrzt, 2)),
-								v_div(cam->vrtc, 2)), vt_mul(cam->normal, -1));
+	// cam->hrzt = vec(cam->viewport_w, 0, 0);
+	// cam->vrtc = vec(0, cam->viewport_h, 0);
+	vup = vec(0, 1, 0);
+	if (cam->normal.x == 0 && fabs(cam->normal.y) == 1
+								&& cam->normal.z == 0)
+		vup = vec(1, 0, 0);
+	cam->hrzt =
+		vt_mul(v_unit(v_cross(cam->normal, vup)), cam->viewport_w);
+	cam->vrtc =
+		vt_mul(v_unit(v_cross(cam->hrzt, cam->normal)), cam->viewport_h);
+	cam->left_bottom =
+		v_sub(v_sub(v_sub(cam->org, v_div(cam->hrzt, 2)),
+		v_div(cam->vrtc, 2)), vt_mul(cam->normal, -1));
+	// cam->left_bottom = v_sub(v_sub(v_sub(cam->org, v_div(cam->hrzt, 2)),
+	// 							v_div(cam->vrtc, 2)), vt_mul(cam->normal, -1));
 	//카메라의 normal값, fov 는 어디서 어떻게 받는걸까?
 	return (cam);
 }
