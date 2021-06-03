5a1e2ab05bf2f1682d6874a346cdc909dc9e58e3
안세현 <sehyeon0814@naver.com>
2021-06-03 15:12:06 +0900

    finish

--- srcs/run.sh
+++ srcs/run.sh
@@ -1,5 +1,3 @@
-# !/bin/bash
-service nginx start
 openssl req -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/OU=sehyan/CN=localhost" -keyout localhost.dev.key -out localhost.dev.crt
 mv localhost.dev.crt etc/ssl/certs/
 mv localhost.dev.key etc/ssl/private/
@@ -8,17 +6,11 @@ chmod 600 etc/ssl/certs/localhost.dev.crt etc/ssl/private/localhost.dev.key
 rm ./etc/nginx/sites-available/default
 cp ./tmp/default etc/nginx/sites-available/
 
-## autoindex
-#change autoindex setting
 if [ ${AUTOINDEX} -eq -1 ] ; then
         sed -i 's/autoindex on;/autoindex off;/g' etc/nginx/sites-available/default
 fi
 
-service nginx restart
-service php7.3-fpm start
-
 cp tmp/phpinfo.php var/www/html/
-service nginx reload
 
 wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.tar.gz
 tar -xvf phpMyAdmin-5.0.2-all-languages.tar.gz
@@ -30,28 +22,22 @@ cp -rp var/www/html/phpmyadmin/config.sample.inc.php var/www/html/phpmyadmin/con
 rm var/www/html/phpmyadmin/config.inc.php
 cp tmp/config.inc.php var/www/html/phpmyadmin/
 
-service nginx reload
-service mysql start
-# service php7.3-fpm restart
-mysql < var/www/html/phpmyadmin/sql/create_tables.sql -u root --skip-password
-mysqladmin -u root -p password
-# service mysql start
-
-echo "CREATE DATABASE IF NOT EXISTS wordpress;" | mysql -u root #--skip-password
-echo "GRANT ALL PRIVILEGES ON *.* TO 'user'@'%' IDENTIFIED BY '1234' WITH GRANT OPTION;" | mysql -u root #--skip-password
-echo "FLUSH PRIVILEGES;" | mysql -u root #--skip-password
-# echo "SHOW DATABASES;" | mysql -u root --skip-password
-
-# service mysql restart
-
 wget https://wordpress.org/latest.tar.gz
 tar -xvf latest.tar.gz
 mv wordpress/ var/www/html/
+cp ./tmp/wp-config.php var/www/html/wordpress/
 chown -R www-data:www-data /var/www/html/wordpress
 
-# cd ../../../../
-# rm ./var/www/html/wordpress/wp-config-sample.php
-cp ./tmp/wp-config.php var/www/html/wordpress/
+service mysql start
+mysql < var/www/html/phpmyadmin/sql/create_tables.sql
 
-service nginx reload
+echo "CREATE DATABASE IF NOT EXISTS wordpress;" | mysql -u root --skip-password
+echo "CREATE USER 'ash'@'localhost' IDENTIFIED BY '1234';" | mysql -u root --skip-password
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'ash'@'localhost';" | mysql -u root --skip-password
+echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
+
+service mysql reload
+service php7.3-fpm start
+service nginx start
+
 bash
