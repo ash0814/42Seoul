b95f934c3303c8968ec811e1eb30877052cd0717
안세현 <sehyeon0814@naver.com>
2021-06-21 14:26:32 +0900

    pust_swap 90

---
+++ push_swap_sub01/push_pop.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_pop.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sehyan <sehyan@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/21 11:43:21 by sehyan            #+#    #+#             */
+/*   Updated: 2021/06/21 11:52:06 by sehyan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	push_head(t_stack *stack, int val)
+{
+	t_node *new;
+
+	if (stack->size == 0)
+	{
+		add_node(stack, val);
+		return ;
+	}
+	if (!(new = (t_node *)malloc(sizeof(t_node))))
+		return ;
+	new->value = val;
+	stack->head->prev = new;
+	new->next = stack->head;
+	stack->head = new;
+	stack->size++;
+}
+
+void	pop_head(t_stack *stack)
+{
+	if (stack->size == 1)
+	{
+		free_node(&stack->head);
+		stack->head = 0;
+		stack->size--;
+		return ;
+	}
+	stack->head = stack->head->next;
+	stack->head->prev->next = NULL;
+	free(stack->head->prev);
+	stack->head->prev = NULL;
+	stack->size--;
+}
+
+void	push_tail(t_stack *stack, int val)
+{
+	t_node *new;
+
+	if (stack->size == 0)
+	{
+		add_node(stack, val);
+		return ;
+	}
+	if (!(new = (t_node *)malloc(sizeof(t_node))))
+		return ;
+	new->value = val;
+	stack->tail->next = new;
+	new->next = NULL;
+	new->prev = stack->tail;
+	stack->tail = new;
+	stack->size++;
+}
+
+void	pop_tail(t_stack *stack)
+{
+	if (stack->size == 1)
+	{
+		free_node(&stack->head);
+		stack->head = 0;
+		stack->size--;
+		return ;
+	}
+	stack->tail = stack->tail->prev;
+	stack->tail->next->prev = NULL;
+	free(stack->tail->next);
+	stack->tail->next = NULL;
+	stack->size--;
+}
