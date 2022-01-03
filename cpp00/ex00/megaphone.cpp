5a672dbd65e6d8559edad8bda408fa76b6fdf8f7
안세현 <sehyeon0814@naver.com>
2022-01-03 20:42:07 +0900

    megaphone

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
 
+int ft_toupper(int c)
+{
+	if ('a' <= c && c <= 'z')
+		c -=32;
+	return c;
+}
+
+int ft_strlen(char *s)
+{
+	int i = 0;
+	while (s[i])
+		i++;
+	return i;
+}
+
 int main(int argc, char **argv)
 {
+	char c;
 	if (argc != 1)
-		std::cout << argv[1] << "\n";
+	{
+		for (int i = 1; i < argc; i++) {
+			for (int j = 0; j < (int)strlen(argv[i]); j++){
+				c = ft_toupper(argv[i][j]);
+				std::cout << c;
+			}
+			std::cout << " ";
+		}
+		std::cout << "\n";
+	}
 	else
-		std::cout << "WOOOOOOOOOOOOOOOOOOOOO\n";
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
 	return (0);
 }
