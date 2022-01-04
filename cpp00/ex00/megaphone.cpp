6a427f743d2d42ea32f389916c0cb022548ca029
안세현 <sehyeon0814@naver.com>
2022-01-04 18:47:41 +0900

    fix: cpp style

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,33 +1,15 @@
 #include <iostream>
 
-int ft_toupper(int c)
-{
-	if ('a' <= c && c <= 'z')
-		c -=32;
-	return c;
-}
-
-int ft_strlen(char *s)
-{
-	int i = 0;
-	while (s[i])
-		i++;
-	return i;
-}
-
 int main(int argc, char **argv)
 {
-	char c;
-	if (argc != 1)
-	{
+	if (argc != 1) {
 		for (int i = 1; i < argc; i++) {
-			for (int j = 0; j < (int)strlen(argv[i]); j++){
-				c = ft_toupper(argv[i][j]);
-				std::cout << c;
+			std::string str(argv[i]);
+			for (size_t j = 0; j < str.length(); j++){
+				std::cout << static_cast<char>(std::toupper(str[j]));
 			}
-			std::cout << " ";
 		}
-		std::cout << "\n";
+		std::cout << std::endl;
 	}
 	else
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
