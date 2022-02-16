01ea6bdc128fc3b2254d78cad54ec0b786452167
ash <sehyeon014@gmail.com>
2022-02-17 00:57:26 +0900

    find, substr, ifstream ofstream, get, write, open ...

--- CPP_Module/cpp01/ex04/main.cpp
+++ CPP_Module/cpp01/ex04/main.cpp
@@ -7,21 +7,32 @@ int main(int argc, char **argv)
 	std::string a;
 	std::string b;
 	std::ifstream ifs;
+	std::ofstream ofs;
 
-	if (argc != 4 || !argv[2][0] || !argv[3][0]) {
+	if (argc != 4 || !argv[2][0] || !argv[3][0])
+	{
 		std::cout << "Input Error" << std::endl;
 		return (1);
 	}
 	a = argv[2];
 	b = argv[3];
-	ifs.open(argv[1], std::ifstream::in);
-	if (ifs.is_open()) {
-		while (!ifs.eof()) {
-			char c;
-			ifs.get(c);
-			std::cout << c;
+	ifs.open(argv[1]);
+	if (ifs.is_open())
+	{
+		ofs.open("replace");
+		char c;
+		std::string str;
+		while (ifs.get(c))
+		{
+			str += c;
 		}
-	} else {
+		// size_t n = str.std::string::find(argv[2], 0);
+		// str.substr(n);
+		if (ofs.is_open())
+			ofs.write(str.c_str(), str.length());
+	}
+	else
+	{
 		std::cout << "Open Error" << std::endl;
 		return (1);
 	}
