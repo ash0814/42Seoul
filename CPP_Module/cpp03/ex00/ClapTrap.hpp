83f9c7963784476247b16299d5eef6a907a8a85c
ash <sehyeon014@gmail.com>
2022-03-01 23:29:36 +0900

    create ex00

---
+++ CPP_Module/cpp03/ex00/ClapTrap.hpp
@@ -0,0 +1,22 @@
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+class ClapTrap
+{
+private:
+	/* data */
+public:
+	ClapTrap(/* args */);
+	~ClapTrap();
+};
+
+ClapTrap::ClapTrap(/* args */)
+{
+}
+
+ClapTrap::~ClapTrap()
+{
+}
+
+
+#endif
