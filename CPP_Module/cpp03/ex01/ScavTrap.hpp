9807938bda1f9a49d12646f249dceb6738bc10e8
ash <sehyeon014@gmail.com>
2022-03-04 01:07:22 +0900

    create ex01

---
+++ CPP_Module/cpp03/ex01/ScavTrap.hpp
@@ -0,0 +1,26 @@
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+#include "./ClapTrap.hpp"
+
+class ScavTrap : ClapTrap
+{
+private:
+	/* data */
+public:
+	ScavTrap(/* args */);
+	~ScavTrap();
+};
+
+ScavTrap::ScavTrap(/* args */)
+{
+}
+
+ScavTrap::~ScavTrap()
+{
+}
+
+
+
+
+#endif
