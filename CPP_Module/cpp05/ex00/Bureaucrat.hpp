4cef78eb60f9a21e28d8bfc516a420347ddcaf68
안세현 <sehyeon0814@naver.com>
2022-03-21 21:03:12 +0900

    create cpp05

---
+++ CPP_Module/cpp05/ex00/Bureaucrat.hpp
@@ -0,0 +1,22 @@
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+class Bureaucrat
+{
+private:
+	/* data */
+public:
+	Bureaucrat(/* args */);
+	~Bureaucrat();
+};
+
+Bureaucrat::Bureaucrat(/* args */)
+{
+}
+
+Bureaucrat::~Bureaucrat()
+{
+}
+
+
+#endif
