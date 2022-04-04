90efc4d76037468efb82f93c59862eb95f87c5ef
안세현 <sehyeon0814@naver.com>
2022-04-04 15:24:02 +0900

    ex01

---
+++ CPP_Module/cpp06/ex01/Data.hpp
@@ -0,0 +1,10 @@
+#ifndef DATA_HPP
+# define DATA_HPP
+
+struct Data
+{
+	int i;
+	char c;
+};
+
+#endif
---
+++ CPP_Module/cpp06/ex01/main.cpp
@@ -0,0 +1,30 @@
+#include "Data.hpp"
+#include <iostream>
+#include <string>
+
+uintptr_t serialize(Data* ptr)
+{
+	return reinterpret_cast<uintptr_t>(ptr);
+}
+
+Data* deserialize(uintptr_t raw)
+{
+	return reinterpret_cast<Data *>(raw);
+}
+
+int main()
+{
+	struct Data *d = new Data();
+
+	d->c = 'S';
+	d->i = 814;
+	uintptr_t uptr = serialize(d);
+	struct Data *newData = deserialize(uptr);
+	std::cout << d << std::endl;
+	std::cout << uptr << std::endl;
+	std::cout << newData << std::endl;
+	std::cout << newData->c << std::endl;
+	std::cout << newData->i << std::endl;
+
+	return (0);
+}
