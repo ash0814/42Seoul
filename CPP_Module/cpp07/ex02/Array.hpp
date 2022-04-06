1c5b14bbc5bc1d9bdf009c28605f5689dd8673bd
안세현 <sehyeon0814@naver.com>
2022-04-06 17:03:26 +0900

    finish: ex02

--- CPP_Module/cpp07/ex02/Array.hpp
+++ CPP_Module/cpp07/ex02/Array.hpp
@@ -1,16 +1,92 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
-template <typename T>
+#include <iostream>
+#include <exception>
+#include <typeinfo>
 
+template <typename T>
 class Array
 {
 private:
-	/* data */
+	T *arrayData;
+	int _size;
 public:
-	Array(/* args */);
+	Array();
+	Array(int n);
+	Array(const Array &a);
 	~Array();
+
+	Array &operator=(const Array &a);
+	T &operator[](int i);
+	const T &operator[](int i) const;
+	int size();
 };
 
+template <typename T>
+Array<T>::Array()
+{
+	arrayData = new T[0];
+	_size = 0;
+}
+
+template <typename T>
+Array<T>::Array(int n)
+{
+	arrayData = new T[n];
+	_size = n;
+}
+
+template <typename T>
+Array<T>::Array(const Array &a)
+{
+	arrayData = new T[a._size];
+	*this = a;
+}
+
+template <typename T>
+Array<T>::~Array()
+{
+	delete [] arrayData;
+}
+
+template <typename T>
+T &Array<T>::operator[](int i)
+{
+	if (i < 0 || i >= _size)
+		throw std::exception();
+	return arrayData[i];
+}
+
+template <typename T>
+const T &Array<T>::operator[](int i) const
+{
+	if (i < 0 || i >= _size)
+		throw std::exception();
+	return arrayData[i];
+}
+
+template <typename T>
+Array<T> &Array<T>::operator=(const Array<T> &a)
+{
+	this->_size = a._size;
+	if (this != &a) {
+		delete this->arrayData;
+		this->arrayData = new T[a._size];
+		for (int i = 0; i < a._size; i++) {
+			this->arrayData[i] = a.arrayData[i];
+		}
+	}
+	return *this;
+}
+
+template <typename T>
+int Array<T>::size()
+{
+	int i = 0;
+	while (this->arrayData[i])
+		i++;
+	return (i);
+}
 
 #endif
