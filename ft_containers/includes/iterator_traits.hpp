57d33c6fe3c5b7a7eacde6e9991bcfd09dbde66f
ash <sehyeon014@gmail.com>
2022-09-07 15:30:02 +0900

    tester: iterator std problems

--- ft_containers/includes/iterator_traits.hpp
+++ ft_containers/includes/iterator_traits.hpp
@@ -6,15 +6,15 @@
 
 namespace ft
 {
-  template <class Iterator> class iterator_traits {
-    typedef typename ft::Iterator::difference_type difference_type;
-    typedef typename ft::Iterator::value_type value_type;
-    typedef typename ft::Iterator::pointer pointer;
-    typedef typename ft::Iterator::reference reference;
-    typedef typename ft::Iterator::iterator_category iterator_category;
+  template <class Iterator> struct iterator_traits {
+    typedef typename Iterator::difference_type difference_type;
+    typedef typename Iterator::value_type value_type;
+    typedef typename Iterator::pointer pointer;
+    typedef typename Iterator::reference reference;
+    typedef typename Iterator::iterator_category iterator_category;
   };
 
-  template <class T> class iterator_traits<T *> {
+  template <class T> struct iterator_traits<T *> {
     typedef std::ptrdiff_t difference_type;
     typedef T value_type;
     typedef T *pointer;
@@ -22,7 +22,7 @@ namespace ft
     typedef ft::random_access_iterator_tag iterator_category;
   };
 
-  template <class T> class iterator_traits<const T *> {
+  template <class T> struct iterator_traits<const T *> {
     typedef std::ptrdiff_t difference_type;
     typedef T value_type;
     typedef const T *pointer;
